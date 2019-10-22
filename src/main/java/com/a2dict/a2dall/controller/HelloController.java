30a8d1eb9d9011cd152096f9ccb103d670796cae
a2dict <a2dict@163.com>
2019-10-22 17:20:29 +0800

    add kotlin

--- src/main/java/com/a2dict/a2dall/controller/HelloController.java
+++ src/main/java/com/a2dict/a2dall/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.a2dict.a2dall.controller;
 
+import com.a2dict.a2dall.HelloKT;
+import com.a2dict.a2dall.Man;
 import com.a2dict.a2dall.domain.People;
 import com.a2dict.a2dall.util.ClojureUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,5 +18,15 @@ public class HelloController {
     public People newPeople() {
         People p = ClojureUtil.loadAndCallFn("a2dall.core", "new-people");
         return p;
+    }
+
+    @RequestMapping("/hello_kt")
+    public Man helloKt() {
+        return new Man("helloKt");
+    }
+
+    @RequestMapping("/echo_kt")
+    public String echo(@RequestParam String msg) {
+        return HelloKT.echo(msg);
     }
 }
---
+++ src/main/kotlin/com/a2dict/a2dall/hello.kt
@@ -0,0 +1,9 @@
+@file:JvmName("HelloKT")
+package com.a2dict.a2dall
+
+/**
+ * Created by zhangmd on 2019/10/22
+ */
+data class Man(val name: String)
+
+fun echo(s: String) = s
